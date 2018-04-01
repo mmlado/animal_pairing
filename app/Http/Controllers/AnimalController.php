5932614d9c5d4097bfe261c5324030b7307894bd
Mladen Milankovic <developer@mmlado.com>
2018-04-01 22:24:03 +0200

    Add base empty controller and views.

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Animal;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('animal.index', [
+
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('animal.create', [
+
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Animal $animal)
+    {
+        return view('animal.view', [
+
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Animal $animal)
+    {
+        return view('animal.edit', [
+
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Animal $animal)
+    {
+        //
+    }
+}
