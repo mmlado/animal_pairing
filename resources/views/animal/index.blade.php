44ed30df26a5e0aad3f7275a79589932fd0cf462
Mladen Milankovic <developer@mmlado.com>
2018-04-06 21:52:37 +0200

    Write out message if there are no animals. Strings put into translation function.

--- resources/views/animal/index.blade.php
+++ resources/views/animal/index.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 @section('content')
+    @if(count($animals) == 0)
+        {{__('There are no animals.')}}
+    @else
     <table class="table table-responsive table-bordered">
         <tr>
-            <th>Name</th>
-            <th>Dob</th>
-            <th>Own</th>
-            <th>Active</th>
-            <th>Options</th>
+            <th>{{__('Name')}}</th>
+            <th>{{__('Dob')}}</th>
+            <th>{{__('Own')}}</th>
+            <th>{{__('Active')}}</th>
+            <th>{{__('Options')}}</th>
         </tr>
-        @each('animal.index.item', $animals, 'animal', 'raw|There are no items.')
+        @each('animal.index.item', $animals, 'animal')
     </table>
     {{$animals->links()}}
+    @endif
 @stop
