5cd030106fe5a745ce003a51e80518b5bc917607
Mladen Milankovic <developer@mmlado.com>
2018-04-07 09:56:19 +0200

    Add link for creating new animal.

--- resources/views/animal/index.blade.php
+++ resources/views/animal/index.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 @section('content')
+    <a href="{{ action('AnimalController@create') }}">{{ __('Add animal') }}</a>
     @if(count($animals) == 0)
-        {{__('There are no animals.')}}
+        {{ __('There are no animals.') }}
     @else
     <table class="table table-responsive table-bordered">
         <tr>
-            <th>{{__('Name')}}</th>
-            <th>{{__('Dob')}}</th>
-            <th>{{__('Own')}}</th>
-            <th>{{__('Active')}}</th>
-            <th>{{__('Options')}}</th>
+            <th>{{ __('Name') }}</th>
+            <th>{{ __('Dob') }}</th>
+            <th>{{ __('Own') }}</th>
+            <th>{{ __('Active') }}</th>
+            <th>{{ __('Options') }}</th>
         </tr>
         @each('animal.index.item', $animals, 'animal')
     </table>
