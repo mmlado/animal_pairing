90f7d7de7774d9a13da59946b5379b0296df5685
Mladen <10460806+mmlado@users.noreply.github.com>
2018-09-17 05:50:42 +0200

    Preparation for reworking of project

--- resources/views/animal/index/options.blade.php
+++
@@ -1,7 +0,0 @@
-<a href="{{ action('AnimalController@show', ['id' => $id]) }}" class="glyphicon glyphicon-eye-open"></a>
-<a href="{{ action('AnimalController@edit', ['id' => $id]) }}" class="glyphicon glyphicon-edit"></a>
-<form action="{{ action('AnimalController@destroy', ['id' => $id]) }}" method="POST" style="display:inline;">
-    {{ method_field('DELETE') }}
-    {{ csrf_field() }}
-    <button class="btn-link glyphicon glyphicon-remove" style="padding:0"></button>
-</form>
