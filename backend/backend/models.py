a394376552b2bb391e315bb1cd97ef337a66d537
Mladen <10460806+mmlado@users.noreply.github.com>
2018-11-19 21:32:25 +0100

    Add gender to animal

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Animal(models.Model):
+    MALE = 'male'
+    FEMALE = 'female'
+
+    GENDER_CHOICES = ((MALE, 'Male'), (FEMALE, 'Female'))
+
     father = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_father")
     mother = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_mother")
     name = models.CharField(max_length = 100)
     dob = models.IntegerField()
+    gender = models.CharField(max_length = 6, choices = GENDER_CHOICES, default = FEMALE)
     active = models.BooleanField()
     own = models.BooleanField()
