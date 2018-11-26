dc6819f67a5f348d260c36c496bc9d116a875ef7
Mladen <10460806+mmlado@users.noreply.github.com>
2018-11-26 20:57:33 +0100

    Change sanitizer to normal model sanitizer, so it sends ids of parents.

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Animal
 
-class AnimalSerializer(serializers.HyperlinkedModelSerializer):
+class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animal
         fields = ('id', 'name', 'dob', 'gender', 'active', 'own', 'father', 'mother')
