6e6c60613180bb3d7e2d019129e57d1a2c33286d
Mladen <10460806+mmlado@users.noreply.github.com>
2018-12-29 14:07:50 +0100

    Add length validator to name. Add dob validator can't be higher than current year.

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MaxLengthValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
+
+def current_year():
+    return datetime.now().year
 
 class Animal(models.Model):
     MALE = 'male'
@@ -8,8 +15,8 @@ class Animal(models.Model):
 
     father = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_father")
     mother = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_mother")
-    name = models.CharField(max_length = 100)
-    dob = models.IntegerField()
+    name = models.CharField(max_length = 100, validators = [MaxLengthValidator(100)])
+    dob = models.IntegerField(validators = [MaxValueValidator(current_year())])
     gender = models.CharField(max_length = 6, choices = GENDER_CHOICES, default = FEMALE)
     active = models.BooleanField()
     own = models.BooleanField()
