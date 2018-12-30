646c3ac19f130c6a206805ada4110879204ed1df
Mladen <10460806+mmlado@users.noreply.github.com>
2018-12-30 15:01:43 +0100

    Remove unsued libraries

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MaxLengthValidator
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxLengthValidator
 
 class Animal(models.Model):
     MALE = 'male'
