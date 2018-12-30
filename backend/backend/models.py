713ad86a770f1f56cfd8c5f6d03c8b485dbd5eef
Mladen <10460806+mmlado@users.noreply.github.com>
2018-12-30 14:56:21 +0100

    Dob future validation moved to business logic

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from datetime import datetime
-
-def current_year():
-    return datetime.now().year
 
 class Animal(models.Model):
     MALE = 'male'
@@ -16,7 +12,7 @@ class Animal(models.Model):
     father = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_father")
     mother = models.ForeignKey("self", null = True, on_delete = models.SET_NULL, related_name = "child_mother")
     name = models.CharField(max_length = 100, validators = [MaxLengthValidator(100)])
-    dob = models.IntegerField(validators = [MaxValueValidator(current_year())])
+    dob = models.IntegerField()
     gender = models.CharField(max_length = 6, choices = GENDER_CHOICES, default = FEMALE)
     active = models.BooleanField()
     own = models.BooleanField()
--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Animal
+from datetime import datetime
 
 class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,14 +17,17 @@ class AnimalSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('The mother has to be female.')
 
     def validate_dob(self, dob):
+        if (dob > datetime.now().year):
+            raise serializers.ValidationError('Animal can\'t be born in the future.')
+
         father_id = self.context['request'].data['father']
         if (father_id):
             father = Animal.objects.get(pk = father_id)
             if (father and father.dob > dob):
-                raise serializers.ValidationError('Animal can\'t be older than it\'s father')
+                raise serializers.ValidationError('Animal can\'t be older than it\'s father.')
 
         mother_id = self.context['request'].data['mother']
         if (mother_id):
             mother = Animal.objects.get(pk = mother_id)
             if (mother and mother.dob > dob):
-                raise serializers.ValidationError('Animal can\'t be older than it\'s mother')
+                raise serializers.ValidationError('Animal can\'t be older than it\'s mother.')
